86a6ae69b3d2ce7f7e39356a3d10c4c050e7ccd7
ihor.pidhornyi <ihor.pidhornyi@smartyads.com>
2023-07-11 17:13:09 +0300

    feat(reactive-forms): reactive forms

--- projects/forms-playground/src/app/core/header/header.component.ts
+++ projects/forms-playground/src/app/core/header/header.component.ts
@@ -1,12 +1,12 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -14,11 +14,13 @@ import {FormsModule} from "@angular/forms";
 export class HeaderComponent implements OnInit {
 
   public searchString = ''
+  public reactiveSearchString = new FormControl('')
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.reactiveSearchString.valueChanges.subscribe(console.log)
   }
 
 }
--- projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
@@ -1,10 +1,13 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {banWords} from "../validators/ban-words.validator";
+import {passwordShouldMatch} from "../validators/password-should-match.validator";
 
 @Component({
   selector: 'app-reactive-forms-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './reactive-forms-page.component.html',
   styleUrls: [
     '../../common-page.scss',
@@ -14,15 +17,65 @@ import { CommonModule } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReactiveFormsPageComponent implements OnInit {
-  
+
+  phoneLabels = ['Main', 'Mobile', 'Work', 'Home'];
+
+  public form = new FormGroup({
+    firstName: new FormControl('Dmytro', [Validators.required, Validators.minLength(2), banWords(['test', 'dummy'])]),
+    lastName: new FormControl('Mezhenskyi', [Validators.required, Validators.minLength(2)]),
+    nickname: new FormControl(
+      '',
+      [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern(/^[\w.]+$/),
+        banWords(['dummy', 'anonymous'])
+      ]
+    ),
+    email: new FormControl('dmytro@decodedfrontend.io', [Validators.email, Validators.required]),
+    yearOfBirth: new FormControl(this.years[this.years.length - 1], Validators.required),
+    passport: new FormControl('', [Validators.pattern(/^[A-Z]{2}[0-9]{6}$/)]),
+    address: new FormGroup({
+      fullAddress: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      postCode: new FormControl(0, Validators.required),
+    }),
+    phones: new FormArray([new FormGroup({
+      label: new FormControl(this.phoneLabels[0]),
+      phone: new FormControl('')
+    })]),
+    password: new FormGroup({
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('')
+    }, {
+      validators: passwordShouldMatch
+    })
+  });
+
   get years() {
     const now = new Date().getUTCFullYear();
     return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
   }
-  
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  addPhone() {
+    this.form.controls.phones.insert(0, new FormGroup({
+      label: new FormControl(this.phoneLabels[0]),
+      phone: new FormControl('')
+    }))
+  }
+
+  removePhone(index: number) {
+    this.form.controls.phones.removeAt(index);
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value)
   }
 
 }
